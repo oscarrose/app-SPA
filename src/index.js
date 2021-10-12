cab2cb9ef841ea30b3ef0f7654c144f818d955a4
oscarrose <brookvivir@gmail.com>
2021-10-12 15:20:32 -0400

    v2 spa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import React from "react";
 import reactDom from "react-dom";
-import {App} from "./App";
 
-reactDom.render(<App/>, document.getElementById("root"));
+import {BrowserRouter, Route,Switch} from "react-router-dom";
+
+//Import the app
+
+import {AppTodoList} from "./App";
+import AppCalculator from "./App";
+import Navbar from "./components/Navbar";
+import {AppCounterClick} from "./App"
+
+
+
+reactDom.render(
+
+    <React.StrictMode>
+        <BrowserRouter>
+        <Navbar/>
+        <Switch>
+            <Route component={AppTodoList} path="/todolist">
+             <AppTodoList/>
+            </Route>
+
+            <Route component={AppCounterClick} path="/counter">
+               <AppCounterClick/>
+              
+            </Route>
+
+            <Route component={AppCalculator} path="/calculator">
+             <AppCalculator/>
+               
+            </Route>
+
+            <Route  path="/gallery">
+             
+            </Route>
+        </Switch>
+        </BrowserRouter>
+    </React.StrictMode>,
+
+     document.getElementById("root")
+);
